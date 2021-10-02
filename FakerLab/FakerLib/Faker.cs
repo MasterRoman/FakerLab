a00433ab373b4b2dbd5fdc8436eb39d204b6c4e2
Roman Kozko <romanka2001@mail.ru>
2021-10-02 13:32:57 +0300

    add basic methods and interface

---
+++ FakerLab/FakerLib/Faker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace FakerLib
+{
+    class Faker : IFaker
+    {
+        public T create<T>()
+        {
+            return (T)createObject(typeof(T));
+        }
+
+        private object createObject(Type type)
+        {
+            object createdObject = null;
+
+            if (type.IsValueType)
+            {
+
+            }
+
+            return createdObject;
+        }
+    }
+}
