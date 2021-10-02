26878bbf4f393a3b99335e502f82efaee14cb497
Roman Kozko <romanka2001@mail.ru>
2021-10-02 22:36:22 +0300

    add list creator

---
+++ FakerLab/ListCreatorLib/ListCreator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using FieldCreators.PrimitiveTypesCreators;
+using FieldCreators;
+
+namespace ListCreatorLib
+{
+    class ListCreator : IGenericCreator
+    {
+
+        public Type curType { get; }
+        private Dictionary<Type, IPrimitiveTypeCreator> primitiveTypeCreator;
+
+        public ListCreator(Dictionary<Type, IPrimitiveTypeCreator> primitiveTypeCreator)
+        {
+            this.curType = typeof(List<>);
+            this.primitiveTypeCreator = primitiveTypeCreator;
+        }
+
+        public object create(Type type)
+        {
+            IList result = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
+            var length = new Random().Next(1, 10);
+
+            if (primitiveTypeCreator.TryGetValue(type, out IPrimitiveTypeCreator creator))
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    result.Add(creator.create());
+                }
+            }
+            else
+            {
+                var defaultValue = "DEFAULT";
+                for (int i = 0; i < length; i++)
+                {
+                    result.Add(defaultValue);
+                }
+            }
+            return result;
+        }
+    }
+}
