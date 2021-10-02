5caccc4c8afc75d59a5f8f819e7fdc6353d01fc3
Roman Kozko <romanka2001@mail.ru>
2021-10-02 15:45:46 +0300

    add base interface for primitive types and realize for bool

---
+++ FakerLab/FieldCreators/PrimitiveTypesCreators/BoolCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldCreators.PrimitiveTypesCreators
+{
+    class BoolCreator : IPrimitiveTypeCreator
+    {
+        public Type curCype { get;}
+
+        public BoolCreator()
+        {
+            this.curCype = typeof(bool);
+        }
+
+        public object create()
+        {
+            var number = new Random().Next();
+            return number % 2 == 0;
+        }
+
+    }
+}
